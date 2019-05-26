905dc0e914bc717f618db198720ab998ae01a024
skoplev <skoplev@gmail.com>
2019-05-26 12:57:36 +0100

    ASCAT TITAN comparison plot.

---
+++ 4/scripts/cmpTitanASCAT.R
@@ -0,0 +1,29 @@
+rm(list=ls())
+
+library(RColorBrewer)
+setwd("~/GoogleDrive/projects/cambridge/ovarianCancerHeterogeneityChemo/repo/HGSOC_TME_Heterogeneity/4")
+
+source("lib/parse.R")
+source("lib/plots.R")
+
+# Load TitanCNA results table
+titan = loadOptClust("data/titanCNA_optimalClusterSolutions.csv", sep=",")
+
+# Load ASCAT
+ascat = fread("data/ASCAT/4_Summary.txt")
+ascat$barcode = sapply(strsplit(ascat$name, "_"), function(x) x[1])
+
+# ascat$CCF[ascat$CCF == 1] = NA   # remove possible outlier
+stopifnot(all(ascat$barcode == titan$barcode))
+
+# Compare ASCAT with TITAN, purity and ploidy
+pdf("plots/ASCAT_TITAN_ploidy_purity.pdf", width=8, height=4.5)
+par(mfrow=c(1, 2))
+scatterPlot(ascat$Ploidy, titan$ploidy, xlab="ASCAT ploidy", ylab="TITAN ploidy",
+	alternative="greater")
+scatterPlot(ascat$CCF, titan$purity, xlab="ASCAT purity", ylab="TITAN purity",
+	method="kendall",
+	alternative="greater")
+dev.off()
+
+
