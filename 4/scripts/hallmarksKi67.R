175188f2519387aca67b1c3019eaabbf6d7d07f3
skoplev <skoplev@gmail.com>
2019-08-23 14:21:59 +0100

    Test for association between Ki67 IF counts and transcriptomic ssGSEA enrichment for G2M checkpoint.

---
+++ 4/scripts/hallmarksKi67.R
@@ -0,0 +1,45 @@
+rm(list=ls())
+
+library(data.table)
+library(RColorBrewer)
+
+setwd("~/GoogleDrive/projects/cambridge/ovarianCancerHeterogeneityChemo/repo/HGSOC_TME_Heterogeneity/4")
+
+source("lib/parse.R")
+source("lib/plots.R")
+
+
+# Load ssGSEA hallmark enrichment results
+hallmarks = fread("data/HallmarksStromaImmune_NES.txt")
+terms = hallmarks$Term
+hallmarks = data.matrix(hallmarks[, -1])
+rownames(hallmarks) = terms
+
+if_frac = fread("data/Ki67/TableS1_TxNaiveData.csv")
+
+
+if_frac_matched = if_frac[match(colnames(hallmarks), if_frac$RNA_WELL), ]
+
+colnames(hallmarks)
+
+
+pathway = "HALLMARK_G2M_CHECKPOINT"
+
+nes = hallmarks[which(rownames(hallmarks) == pathway), ]
+
+pdf(paste0("plots/ssGSEA_", pathway, "_Ki67.pdf"), width=4.0, height=4.5)
+scatterPlot(if_frac_matched$Ki67, nes,
+	xlab="Ki67 (% positive cells)",
+	ylab=paste0(pathway, " (NES, mRNA ssGSEA)"),
+	method="spearman",
+	bg=brewer.pal(9, "Pastel1")[1],
+	pch=21)
+dev.off()
+
+
+# idx = nes > 0.45
+# sum(idx)
+
+# scatterPlot(if_frac_matched$Ki67[!idx], nes[!idx], method="spearman")
+# if_frac_matched[idx, ]
+# scatterPlot(if_frac_matched$Ki67, nes, method="kendall")
