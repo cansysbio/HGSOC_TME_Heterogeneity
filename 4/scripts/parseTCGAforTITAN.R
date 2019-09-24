b3ffd4d970e0befec13dd1f18d1130f322e1b561
skoplev <skoplev@gmail.com>
2019-09-24 15:13:57 +0100

    Parsing of TCGA BAM files for extended set of OV tumor samples.

--- 4/scripts/parseTCGAforTITAN.R
+++ 4/scripts/parseTCGAforTITAN.R
@@ -3,52 +3,90 @@ rm(list=ls())
 library(stringr)
 library(TCGAutils)
 library(stringr)
+library(dplyr)
 
 setwd("~/GoogleDrive/projects/cambridge/ovarianCancerHeterogeneityChemo/repo/HGSOC_TME_Heterogeneity/4")
 
-# TCGA whole exome data downloaded by Danish Memon, stored at
-# /data/memon01/tcga/TCGA-OV
-# including the 'manifest.txt' file describing available .bam files
 
-samples = read.table(
-	# "data/TCGA/manifest.txt",  # hg38
-	"data/TCGA/gdc_manifest.2019-08-01.txt",  # hg19
-	stringsAsFactors=FALSE,
-	header=TRUE)
+# bam_set = "original"  # Original set of BAM files downloaded by Danish
+bam_set = "extended"  # Extended BAM files
 
 
-samples$path = paste0("/data/memon01/tcga/TCGA-OV-LEGACY/", samples$id, "/", samples$filename)
+if (bam_set == "original") {
+	# TCGA whole exome data downloaded by Danish Memon, stored at
+	# /data/memon01/tcga/TCGA-OV
+	# including the 'manifest.txt' file describing available .bam files
 
-write(samples$path, "data/TCGA/sample_paths.txt")  # for
+	samples = read.table(
+		# "data/TCGA/manifest.txt",  # hg38
+		"data/TCGA/gdc_manifest.2019-08-01.txt",  # hg19
+		stringsAsFactors=FALSE,
+		header=TRUE)
+
+
+	samples$path = paste0("/data/memon01/tcga/TCGA-OV-LEGACY/", samples$id, "/", samples$filename)
+
+	# write(samples$path, "data/TCGA/sample_paths.txt")  # for
+
+} else if (bam_set == "extended") {
+	# ADD
+	samples1 = read.table(
+		"data/TCGA/gdc_manifest.2019-09-13.add.txt",
+		stringsAsFactors=FALSE,
+		header=TRUE)
+
+	samples1$path = paste0("/data/memon01/tcga/TCGA-OV-LEGACY-ADD/", samples1$id, "/", samples1$filename)
+
+	# WGS
+	samples2 = read.table(
+		"data/TCGA/gdc_manifest.wgs.txt",
+		stringsAsFactors=FALSE,
+		header=TRUE)
+
+	samples2$path = paste0("/data/memon01/tcga/TCGA-OV-LEGACY-WITH-WGS/", samples2$id, "/", samples2$filename)
+
+	samples = rbind(samples1, samples2)
+
+} else {
+	stop("Unrecognized bam_set value")
+}
+
 
 # Load tests for NCBI ('1') or USCF ('chr1') format
 # From running testChrFormat.sh on list of sample paths, on mmlab cluster
-samples_info = read.csv("data/TCGA/bamfile_info.csv")
+# samples_info = read.csv("data/TCGA/bamfile_info.csv")
+samples_info = read.csv("data/TCGA/bamfile_info_add_wgs.csv")
 
 
 samples_info$reference = str_match(samples_info$header, "AS:(.*?)\\s+")[, 2]
 table(samples_info$reference)
 
 
-
-stopifnot(samples_info$path == samples$path)
-
-samples$chrom_counts = samples_info$chr_count
-samples$reference = samples_info$reference
+# Inner join, only include paths contained in both tables
+# This ensures that bam files have been downloaded (confirmed in samples_info), and that
+# they are specificed in the provided samples table
+samples = merge(samples, samples_info, by="path")
 
 
 # # Exclude USCF chromosome coodinates
-# message("Excluding samples with UCSF chromosome coordinates, n=", sum(samples$chrom_counts > 0))
-# samples = samples[samples$chrom_counts == 0, ]
+# message("Excluding samples with UCSF chromosome coordinates, n=", sum(samples$chr_count > 0))
+# samples = samples[samples$chr_count == 0, ]
 
 # Also excluding non GRCh37 reference genomes...
-idx = samples$chrom_counts == 0 & samples_info$reference %in% c("GRCh37", "GRCh37-lite")
+idx = samples$chr_count == 0 & samples$reference %in% c("GRCh37", "GRCh37-lite")
 message("Excluding samples, n=", sum(!idx))
 samples = samples[idx, ]
 
 
+samples = samples[!is.na(samples$path), ]
+
 # Get TCGA barcodes from file names
-samples$tcga_id = filenameToBarcode(samples$filename, legacy=TRUE)[, 3]
+samples = merge(samples,
+	rename(filenameToBarcode(samples$filename, legacy=TRUE),
+		filename=file_name,
+		tcga_id=aliquots.submitter_id),
+	by="filename")
+
 
 table(table(samples$tcga_id))
 
@@ -76,7 +114,6 @@ samples$tcga_patient_id = str_match(samples$tcga_id, "^(TCGA-[:alnum:]+-[:alnum:
 samples$match_group = paste0(samples$tcga_patient_id, "_", samples$analyte)
 
 
-
 sample_pairs = sapply(unique(samples$match_group), function(group) {
 	idx = samples$match_group == group
 
@@ -92,7 +129,6 @@ sample_pairs = t(sample_pairs)
 colnames(sample_pairs) = c("tumor", "blood")
 sample_pairs = data.frame(sample_pairs)
 
-write.csv(sample_pairs, file="data/TCGA/sample_pairs.csv")  # also contains unmatched TCGA barcodes, for manual inspection
 
 
 # Use only complete pairs
@@ -112,42 +148,46 @@ sample_pairs_compl$tumor_ref = samples$reference[match(sample_pairs_compl$tumor,
 sample_pairs_compl$blood_ref = samples$reference[match(sample_pairs_compl$blood, samples$tcga_id)]
 
 
-write.csv(sample_pairs_compl, file="data/TCGA/sample_pairs_compl.csv", row.names=FALSE, quote=FALSE)
-write(
-	c(sample_pairs_compl$tumor_path, sample_pairs_compl$blood_path),
-	file="data/TCGA/sample_pairs_paths.txt")
+if (bam_set == "original") {
+	write.csv(sample_pairs, file="data/TCGA/sample_pairs.csv")  # also contains unmatched TCGA barcodes, for manual inspection
 
-write(
-	dirname(c(sample_pairs_compl$tumor_path, sample_pairs_compl$blood_path)),
-	file="data/TCGA/sample_pairs_dirs.txt")
+	write.csv(sample_pairs_compl, file="data/TCGA/sample_pairs_compl.csv", row.names=FALSE, quote=FALSE)
+	write(
+		c(sample_pairs_compl$tumor_path, sample_pairs_compl$blood_path),
+		file="data/TCGA/sample_pairs_paths.txt")
 
-
-# Write as .yaml sample file for use as input to TITAN
-# -------------------------------------
-yaml_file = "config/tcga_samples.yaml"
-
-yaml_indent = "  "
-
-# File paths of all available samples
-write("samples:",
-	file=yaml_file)
-
-write(
-	# paste0(yaml_indent, samples$tcga_id, ": ", "/data/memon01/tcga/TCGA-OV-LEGACY/", samples$id, "/", samples$filename),
-	paste0(yaml_indent, samples_included$tcga_id, ": ", "/data/memon01/tcga/TCGA-OV-LEGACY/", samples_included$id, "/", samples_included$filename),
-	append=TRUE,
-	file=yaml_file)
-
-# Tumor-normal pairs
-write("pairings:",
-	append=TRUE,
-	file=yaml_file)
-write(
-	paste0(yaml_indent, sample_pairs_compl$tumor, ": ", sample_pairs_compl$blood),
-	append=TRUE,
-	file=yaml_file)
+	write(
+		dirname(c(sample_pairs_compl$tumor_path, sample_pairs_compl$blood_path)),
+		file="data/TCGA/sample_pairs_dirs.txt")
 
 
+	# Write as .yaml sample file for use as input to TITAN
+	# -------------------------------------
+	yaml_file = "config/tcga_samples.yaml"
+
+	yaml_indent = "  "
+
+	# File paths of all available samples
+	write("samples:",
+		file=yaml_file)
+
+	write(
+		# paste0(yaml_indent, samples$tcga_id, ": ", "/data/memon01/tcga/TCGA-OV-LEGACY/", samples$id, "/", samples$filename),
+		paste0(yaml_indent, samples_included$tcga_id, ": ", "/data/memon01/tcga/TCGA-OV-LEGACY/", samples_included$id, "/", samples_included$filename),
+		append=TRUE,
+		file=yaml_file)
+
+	# Tumor-normal pairs
+	write("pairings:",
+		append=TRUE,
+		file=yaml_file)
+	write(
+		paste0(yaml_indent, sample_pairs_compl$tumor, ": ", sample_pairs_compl$blood),
+		append=TRUE,
+		file=yaml_file)
+} else if (bam_set == "extended") {
+	# TODO
+}
 
 # # Some examples
 samples[samples$tcga_id == "TCGA-61-2610-02A-01W-1092-09", ]
