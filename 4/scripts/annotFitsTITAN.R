a3cc6ba4aa4f48842aad589f6ea71f540530aa6d
skoplev <skoplev@gmail.com>
2019-12-11 11:17:55 +0000

    More formating of TITAN copy-number fit QC criteria.

--- 4/scripts/annotFitsTITAN.R
+++ 4/scripts/annotFitsTITAN.R
@@ -12,3 +12,14 @@ titan$QC = titan$barcode %in% qc_filter
 
 write.csv(titan, "data/QC/titanCNA_optimalClusterSolutionsQC.csv", row.names=FALSE)
 	
+
+
+# Mannually annotated table format
+# -------------------------
+titan_clean = fread("data/QC/titanCNA_optimalClusterSolutionsQCmanual_clean.csv")
+
+filter_matrix_geoff = readRDS("data/QC/filter_matrix.rds")
+filter_matrix_geoff$barcode = rownames(filter_matrix_geoff)
+
+tab = merge(titan_clean, filter_matrix_geoff, by="barcode")
+write.csv(tab, "data/QC/titanCNA_QC.csv", row.names=FALSE)
